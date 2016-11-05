7ec375d1aebcbe707c257532bec1eee54b141ba0
vsavkin <avix1000@gmail.com>
2016-11-05 14:47:26 -0700

    remove unused import

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import {NgModule, Component} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {Router, RouterModule, UrlHandlingStrategy} from '@angular/router';
 import {BrowserModule} from '@angular/platform-browser';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
